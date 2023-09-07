3bc1fea263a6bf34ea68c75464b9f730bde329fd
joshawuro <joshawuro@gmail.com>
2023-09-07 09:02:38 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /*
- * main - Entry point takes void as an argument
+ * main - Entry point 
  * Return: Always 0 (success)
  */
-int main(void)
+int main(void) /*void indicates that the main take no argument*/
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
