d26550b4b0b90088aa4693d613656d1d1b756e17
joshawuro <joshawuro@gmail.com>
2023-09-11 06:57:44 -0700

    done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,9 @@
 
 /*
  * main - produces random number and checks the last digit
+ * @n: is a variable that stores the random number generated
  *
- * return: always 0
+ * Return: always 0
  */
 int main(void)
 {
