fc255e2c971a9cf1a1fafc355cf97899ced0334d
joshawuro <joshawuro@gmail.com>
2023-09-11 19:05:50 -0700

    betty done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point for the code
  *
  * Return: success (0)
  */
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(str);
 
