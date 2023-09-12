08c4bae11d799a99cef09e3b6ecc8d95aac03823
joshawuro <joshawuro@gmail.com>
2023-09-11 19:26:19 -0700

    betty checks done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,10 @@
 /**
  * main - entry point
  *
- * return: always 0 (success)
+ * Return: always 0 (success)
  */
-int main(void){
+int main(void)
+{
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
