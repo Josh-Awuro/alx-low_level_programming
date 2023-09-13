9b5fba9011568e19fbb1b07da9063dca5b47008f
joshawuro <joshawuro@gmail.com>
2023-09-12 19:41:19 -0700

    done

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,7 +4,8 @@
 
 /**
  * _abs - prints the absolute value of an integer value
- *
+ * @j: is an integer data type that holds either
+ * negative or positive numbers
  * Return: the absolute value of the integer value
  */
 int _abs(int j)
