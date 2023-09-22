d1d96ec4c7b1506100585e0ca053a0d267be51cb
joshawuro <joshawuro@gmail.com>
2023-09-22 03:51:10 +0000

    done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - take a pointer to an int an updates to 98
+ * 
+ * Return: void
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This project is aimed at pointers and arrays in c.
