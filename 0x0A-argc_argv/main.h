c65e4d3e7d290b208ee74eff3e7e004eae77a231
joshawuro <joshawuro@gmail.com>
2023-09-30 00:07:09 +0000

    done

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int _putchar(char c);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
+int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int argc, char *argv[]);
+
+#endif
